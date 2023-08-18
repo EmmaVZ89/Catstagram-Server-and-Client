076c90b5a3246ca6b92c61e2c4df273a868a166d
Emmanuel Zelarayan <emmavz@live.com>
2023-08-17 22:11:51 -0300

    add create post functionality and implement canActivate

--- catstagram/src/app/app.module.ts
+++ catstagram/src/app/app.module.ts
@@ -8,16 +8,19 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { CatService } from './services/cat.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, CreatepostComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, CatService, AuthGuardService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
